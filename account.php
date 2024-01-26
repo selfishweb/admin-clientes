63dbd50803a2b9feb2c27f413b8180f6c42e95f9
Yair <vyair3882@gmail.com>
2024-01-26 15:27:17 -0600

    Update account.php

--- account.php
+++ account.php
@@ -4,8 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="https://cdn.selfish.com.mx/wp-content/uploads/2016/09/ico-2.png" type="image/x-icon">
+  <link rel="stylesheet" href="https://clientes.selfish.com.mx/css/style.css">
   <link rel="stylesheet" href="css/account.css">
-  <title>Sistema de Cliente | Mi Cuenta</title>
+  <title>Sistema de Admin | Mi Cuenta</title>
 </head>
 <body>
 
